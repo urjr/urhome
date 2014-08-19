3842e7d88e6c8e821606891e4b28687e4a25b59b
Ulises Reyes <urjr8891@gmail.com>
2014-08-19 12:39:35 -0400

    updated routes to nest photos in projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :photos
-
-  resources :projects
+  resources :projects do
+    resources :photos
+  end
 
   resources :categories
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818215533) do
+ActiveRecord::Schema.define(version: 20140819163532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20140818215533) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "photos", force: true do |t|
+    t.string   "caption"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "photos", ["project_id"], name: "index_photos_on_project_id", using: :btree
 
   create_table "projects", force: true do |t|
     t.string   "name"
