9769d5e54e80ed22c330f598ff8b22066b43e70f
Ulises Reyes <urjr8891@gmail.com>
2014-08-20 12:52:02 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :categories
 
-  root 'welcome#index'
+  root 'categories#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
